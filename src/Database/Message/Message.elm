04bcbe710ba2816f055f16073efd00ef937875f0
SidneyNemzer <sidney@ucode.com>
2017-10-15 22:22:25 -0700

    Add message type decoder and decodeBase

--- src/Database/Message/Message.elm
+++ src/Database/Message/Message.elm
@@ -1,5 +1,6 @@
 module Database.Message.Message exposing (MessageType(..), WithRequestNumber, encodeBase)
 
+import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode exposing (Value)
 
 
@@ -22,9 +23,28 @@ encodeMessageType messageType =
             "d"
 
 
+decodeMessageType : String -> Decoder MessageType
+decodeMessageType messageType =
+    case messageType of
+        "c" ->
+            Decode.succeed Control
+
+        "d" ->
+            Decode.succeed Database
+
+        _ ->
+            Decode.fail <| "Unknown message type '" ++ messageType ++ "'"
+
+
 encodeBase : MessageType -> List ( String, Value ) -> Value
 encodeBase messageType data =
     Encode.object
         [ ( "t", Encode.string <| encodeMessageType messageType )
         , ( "d", Encode.object data )
         ]
+
+
+decodeBase : Decoder MessageType
+decodeBase =
+    Decode.field "t" Decode.string
+        |> Decode.andThen decodeMessageType
