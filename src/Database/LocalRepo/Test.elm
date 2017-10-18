b6ea023889e2c52a853af37e239aed7cba8de032
SidneyNemzer <sidney@ucode.com>
2017-10-17 18:12:55 -0700

    Update test

--- src/Database/LocalRepo/Test.elm
+++ src/Database/LocalRepo/Test.elm
@@ -1,14 +1,23 @@
 module Database.LocalRepo.Test exposing (main)
 
 import Html exposing (Html, div, text)
-import Database.LocalRepo.LocalRepo as LocalRepo
+import Database.LocalRepo.LocalRepo as LocalRepo exposing (Value)
 
 
-myList : List String
-myList =
-    []
+database : Value
+database =
+    LocalRepo.set [ "a", "b", "c" ] LocalRepo.empty (LocalRepo.Bool True)
+
+
+lines : List String -> List (Html Never)
+lines =
+    List.map (\lineText -> div [] [ text lineText ])
 
 
 main : Html Never
 main =
-    div [] [ text <| toString <| LocalRepo.heads myList ]
+    div [] <|
+        lines
+            [ toString <| LocalRepo.get [ "a" ] database
+            , toString <| database
+            ]
