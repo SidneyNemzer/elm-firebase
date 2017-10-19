98dcb6f0e4fc2338d463279ea15dc65dc8e97aae
SidneyNemzer <sidney@ucode.com>
2017-10-18 22:19:39 -0700

    Implement update, toJson, and toString

--- src/Database/LocalRepo/LocalRepo.elm
+++ src/Database/LocalRepo/LocalRepo.elm
@@ -1,6 +1,7 @@
-module Database.LocalRepo.LocalRepo exposing (LocalRepo, Value(..), Path, heads, empty, set, get)
+module Database.LocalRepo.LocalRepo exposing (LocalRepo, Value(..), Path, empty, set, get, update, toJson, toString)
 
 import Dict exposing (Dict)
+import Json.Encode as Encode
 
 
 type Value
@@ -34,6 +35,16 @@ pathFromUri =
     String.split "/"
 
 
+combineTuble2 : ( Maybe a, Maybe b ) -> Maybe ( a, b )
+combineTuble2 tuple =
+    case tuple of
+        ( Just a, Just b ) ->
+            Just ( a, b )
+
+        ( _, _ ) ->
+            Nothing
+
+
 get : Path -> Value -> Value
 get path repo =
     case repo of
@@ -62,61 +73,104 @@ get path repo =
             Null
 
 
-heads : List a -> Maybe ( List a, a )
-heads list =
-    let
-        reversed =
-            List.reverse list
-
-        maybeBoth =
-            ( List.tail reversed |> Maybe.map List.reverse
-            , List.head reversed
-            )
-    in
-        case maybeBoth of
-            ( Just a, Just b ) ->
-                Just ( a, b )
-
-            ( _, _ ) ->
-                Nothing
-
-
-combineTuble2 : ( Maybe a, Maybe b ) -> Maybe ( a, b )
-combineTuble2 tuple =
-    case tuple of
-        ( Just a, Just b ) ->
-            Just ( a, b )
-
-        ( _, _ ) ->
-            Nothing
-
-
-set : Path -> LocalRepo -> Value -> Value
+set : Path -> LocalRepo -> Value -> LocalRepo
 set path repo value =
     case combineTuble2 ( List.head path, List.tail path ) of
         -- Set the value in the current tree
         Just ( segment, [] ) ->
-            Tree <| Dict.insert segment value repo
+            Dict.insert segment value repo
 
         -- Find the next segment of the current tree
         Just ( segment, remainingPath ) ->
             case Dict.get segment repo of
                 -- Update the tree and continue down
                 Just (Tree tree) ->
-                    Tree <|
-                        Dict.insert
-                            segment
-                            (set remainingPath tree value)
-                            repo
+                    Dict.insert
+                        segment
+                        (Tree <| set remainingPath tree value)
+                        repo
 
                 -- Value isn't a tree, overwrite and continue down
                 _ ->
-                    Tree <|
-                        Dict.insert
-                            segment
-                            (set remainingPath Dict.empty value)
-                            repo
+                    Dict.insert
+                        segment
+                        (Tree <| set remainingPath Dict.empty value)
+                        repo
 
         -- Path is empty
         Nothing ->
-            value
+            Debug.crash "Invalid path"
+
+
+{-| Combines two trees, overwriting the values of the first if necesarry
+-}
+update : LocalRepo -> LocalRepo -> LocalRepo
+update baseTree updateTree =
+    Dict.merge
+        Dict.insert
+        (\segment baseValue updateValue resultTree ->
+            case ( baseValue, updateValue ) of
+                ( Tree tree1, Tree tree2 ) ->
+                    Dict.insert segment (Tree <| update tree1 tree2) resultTree
+
+                _ ->
+                    Dict.insert segment (Tree updateTree) resultTree
+        )
+        Dict.insert
+        baseTree
+        updateTree
+        Dict.empty
+
+
+encodeTree : LocalRepo -> Encode.Value
+encodeTree =
+    Dict.toList
+        >> List.map
+            (\( key, value ) ->
+                case value of
+                    Tree tree ->
+                        ( key, encodeTree tree )
+
+                    String string ->
+                        ( key, Encode.string string )
+
+                    Int int ->
+                        ( key, Encode.int int )
+
+                    Float float ->
+                        ( key, Encode.float float )
+
+                    Bool bool ->
+                        ( key, Encode.bool bool )
+
+                    Null ->
+                        ( key, Encode.null )
+            )
+        >> Encode.object
+
+
+toJson : LocalRepo -> String
+toJson =
+    encodeTree >> Encode.encode 2
+
+
+toString : Value -> String
+toString value =
+    case value of
+        Tree tree ->
+            toJson tree
+
+        String string ->
+            string
+
+        Int int ->
+            Basics.toString int
+
+        Float float ->
+            Basics.toString float
+
+        Bool bool ->
+            Basics.toString bool
+
+        Null ->
+            "null"
--- src/Database/LocalRepo/Test.elm
+++ src/Database/LocalRepo/Test.elm
@@ -1,12 +1,18 @@
 module Database.LocalRepo.Test exposing (main)
 
 import Html exposing (Html, div, text)
-import Database.LocalRepo.LocalRepo as LocalRepo exposing (Value)
+import Database.LocalRepo.LocalRepo as LocalRepo exposing (Value(..), LocalRepo)
 
 
-database : Value
+database : LocalRepo
 database =
     LocalRepo.set [ "a", "b", "c" ] LocalRepo.empty (LocalRepo.Bool True)
+        |> (\repo -> LocalRepo.set [ "a", "m", "l" ] repo (Bool True))
+
+
+updateDatabase : LocalRepo
+updateDatabase =
+    LocalRepo.set [ "a", "b", "d", "e" ] LocalRepo.empty (LocalRepo.Bool True)
 
 
 lines : List String -> List (Html Never)
@@ -14,10 +20,15 @@ lines =
     List.map (\lineText -> div [] [ text lineText ])
 
 
+pre : String -> Html Never
+pre =
+    text >> List.singleton >> Html.pre []
+
+
 main : Html Never
 main =
-    div [] <|
-        lines
-            [ toString <| LocalRepo.get [ "a" ] database
-            , toString <| database
-            ]
+    div []
+        [ pre <| LocalRepo.toJson <| database
+        , pre <| LocalRepo.toString <| LocalRepo.get [ "a" ] (Tree database)
+        , pre <| LocalRepo.toJson <| LocalRepo.update database updateDatabase
+        ]
